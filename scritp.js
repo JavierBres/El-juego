dc62f87337709df6673f7761c5bd6e809b97b0a2
Perezdh13 <Perezdh13@gmail.com>
2022-12-21 08:02:41 +0100

    add container

---
+++ scritp.js
@@ -0,0 +1,7 @@
+// const gamers = document.getElementById("gamer")
+// let player["gamers"]
+// const jugadores = player.push("uname")
+// console.log(player)
+
+const element = [...document.getElementsByClassName("card")];
+console.dir (element) ;
