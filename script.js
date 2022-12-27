c18c0767b3e4c4de11d78bb46aa27a90adabcd43
JavierBP <fjavierbresperez@yahoo.es>
2022-12-27 13:02:15 +0100

    update all files

---
+++ script.js
@@ -0,0 +1,7 @@
+function pageGame() {   
+    window.location.href="/index2.html";    
+}
+
+function popUp(URL) {
+    window.open(URL, 'Nombre de la ventana', 'toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=1,width=300,height=200,left = 390,top = 50');
+}
