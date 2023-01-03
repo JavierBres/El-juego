884b7dc2ca2133a3a03a912f6a61e432acaea772
Perezdh13 <Perezdh13@gmail.com>
2023-01-03 14:42:18 +0100

    commit 3/01/23

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
-
 // Crea un botón "cerrar" y añádelo a cada elemento de la lista
-var myNodelist = document.getElementsByTagName("LI");
+var myNodelist = document.getElementsByTagName("li");
 var i;
 for (i = 0; i < myNodelist.length; i++) {
   var span = document.createElement("SPAN");
@@ -20,13 +19,7 @@ for (i = 0; i < close.length; i++) {
   }
 }
 
-// Agregue un símbolo de "marcado" al hacer clic en un elemento de la lista
-var list = document.querySelector('ol');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
+
 
 // Crear un nuevo elemento de lista al hacer clic en el botón "Agregar"
 function newElement() {
@@ -50,8 +43,42 @@ function newElement() {
     }
   }
 }
-const N = [ "Javier", "Elena", "Pablo", "Lucía"];
-const R = []
+
+
+let Name = [];
+
+// function getDataFromList(id) {
+//   let data = [...document.querySelectorAll(`#${id} li`)]
+//     .map(element => element.innerText);
+
+//   console.log(data);
+// }
+
+// getDataFromList('myOL');
+// console.info(Name)
+
+function añadir() {
+  let player = document.getElementById('myInput').value.split(" ");
+  Name = Name.concat(player);
+  document.getElementById('myInput').value = ""; 
+  console.info(Name);
+  document.getElementById("myOL").innerHTML = Name.join("<li>")
+  }
+ 
+   
+  //   let list = document.getElementById("myOL");
+  //  for( i = 0; i < Name.length; i++){
+  //   var li = document.createElement("li");
+  //   li.innerText= Name[i];
+  //   list.appendChild(li);
+  //   };
+  
+
+
+
+
+const dead = [];
+
 
 const playclick = document.getElementById("playclick");
 
@@ -59,14 +86,17 @@ playclick.addEventListener("click", RespondClick);
 
 
 function RespondClick(){
-    aleatorio = N [Math.floor(Math.random() * N.length)];
+    aleatorio = Name [Math.floor(Math.random() * Name.length)];
     
-    while (R.includes(aleatorio)){
-        aleatorio = N [Math.floor(Math.random() * N.length)]; 
+    while (dead.includes(aleatorio)){
+        aleatorio = Name [Math.floor(Math.random() * Name.length)]; 
     } 
-        R.push(aleatorio);
-  console.info (R);
-    if (R.length == N.length){
+        dead.push(aleatorio);
+  console.info (aleatorio);
+    if (dead.length == Name.length){
         alert("Fin del juego");
     }
 }
+
+
+
