5016bd000f02ca9be701a97dcb0ea861eeee9c15
Pablo <perezdh13@gmail.com>
2023-01-04 13:22:41 +0100

    clean code

--- script.js
+++ script.js
@@ -10,18 +10,19 @@ for (i = 0; i < myNodelist.length; i++) {
 }
 
 // Haga clic en un botón de cierre para ocultar el elemento de la lista actual
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
+// var close = document.getElementsByClassName("close");
+// var i;
+// for (i = 0; i < close.length; i++) {
+//   close[i].onclick = function () {
+//     var div = this.parentElement;
+//     div.style.display = "none";
+//   }
+// }
 
 
 
 // Crear un nuevo elemento de lista al hacer clic en el botón "Agregar"
+let lives = ["javi","elena","lucia","pablo","diego"];
 function newElement() {
   var li = document.createElement("li");
   var inputValue = document.getElementById("myInput").value;
@@ -29,6 +30,7 @@ function newElement() {
   li.appendChild(t);
   document.getElementById("myOL").appendChild(li);
   document.getElementById("myInput").value = "";
+  lives.push(inputValue)
 
   var span = document.createElement("SPAN");
   var txt = document.createTextNode("\u00D7");
@@ -37,73 +39,50 @@ function newElement() {
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
     }
   }
+  console.info(lives)
 }
 
-
-let Name = [];
-// let Name = document.getElementById("myOL");
-// let items = document.getElementsByTagName("li");
-
-// for (let i=0; i<items.length; ++i) {
-//   items[i].innerHTML
-// }
-
-
-// function getDataFromList(id) {
-//   let data = [...document.querySelectorAll(`#${id} li`)]
-//     .map(element => element.innerText);
-
-//   console.log(data);
-// }
-
-// getDataFromList('myOL');
-
-
-function añadir() {
-  let player = document.getElementById('myInput').value.split(" ");
-  Name = Name.concat(player);
-  document.getElementById('myInput').value = ""; 
-  console.info(Name);
-  document.getElementById("myOL").innerHTML = Name.join("<li>")
-  }
- 
-   
-  //   let list = document.getElementById("myOL");
-  //  for( i = 0; i < Name.length; i++){
-  //   var li = document.createElement("li");
-  //   li.innerText= Name[i];
-  //   list.appendChild(li);
-  //   };
-  
 
 
 
 
 const dead = [];
 
-
 const playclick = document.getElementById("playclick");
 
 playclick.addEventListener("click", RespondClick);
 
-
-function RespondClick(){
-    aleatorio = Name [Math.floor(Math.random() * Name.length)];
-    
-    while (dead.includes(aleatorio)){
-        aleatorio = Name [Math.floor(Math.random() * Name.length)]; 
-    } 
-        dead.push(aleatorio);
-  console.info (aleatorio);
-    if (dead.length == Name.length){
-        alert("Fin del juego");
-    }
+         
+function RespondClick() {
+  aleatorio = lives[Math.floor(Math.random() * lives.length)];
+  
+  while (dead.includes(aleatorio)) {
+    aleatorio = lives[Math.floor(Math.random() * lives.length)];
+  }
+  dead.push(aleatorio);
+  
+  console.info(dead);
+  
+  document.getElementById("muerto").innerHTML = aleatorio;
+  
+  if (dead.length === lives.length) {
+    setTimeout (()=>{alert("Fin del juego")},1000);
+  }
 }
+
+// function deleteLives (itemLive){
+
+// }
+
+ console.info(lives)
+ 
+
+
 
 
 
